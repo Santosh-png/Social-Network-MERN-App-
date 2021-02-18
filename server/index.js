f6f561171fe797f59c806ae367691f0e144c4d4b
unknown <santoshbist69@gmail.com>
2021-02-18 16:36:20 +0545

    Initial Commit: Create project

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) =>{
+    res.send("Hello server running at port 5000");
+})
+app.listen(5000);
+
+console.log("Server running at port 5000");
